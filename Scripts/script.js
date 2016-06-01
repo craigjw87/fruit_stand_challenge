27257bcc57d6db040e504fd8b5ef7aafbb84933b
Craig <Craig@Craigs-MacBook-Pro-2.local>
2016-06-01 15:59:08 -0500

    Primary commit

---
+++ Scripts/script.js
@@ -0,0 +1,20 @@
+var fruitStand = [];
+var timeoutID = window.setTimeout(randomNumber, 15000);
+function randomNumber(min, max){
+  return (Math.random() * (max-min) + min);
+}
+
+
+
+
+// $(document).ready(function(){
+// for (var i = 0; i < fruitStand.length; i++) {
+//
+//   }
+//   fruitStand[i]
+//   };
+// };
+
+
+
+console.log(Number(randomNumber(-0.50, 0.50).toFixed(2)));
